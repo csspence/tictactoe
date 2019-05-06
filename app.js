263c8a4f580656f677788310c377121b1b824471
Crew Spence <csspence123@gmail.com>
2019-05-06 13:34:27 -0500

    Nonfunctional tic tac toe board is showing on screen with buttons that throw alerts.

---
+++ app.js
@@ -0,0 +1,31 @@
+//a function that changes a cell when it is clicked (first move is always x)
+  //only empty cells can be clicked (use a boolean)
+  //
+
+const takeTurn = () => {
+  
+  document.getElementById('tag-id').innerHTML = '<ol><li>html data</li></ol>'; 
+};
+
+
+
+//a function that checks if there are any wins in a row 
+
+
+
+
+//a function that checks if there are any wins in a column
+
+
+
+
+//a function that checks if there are any wins diagnonally
+
+
+
+
+//a function that iterates through the board and replaces each cell with null
+
+
+
+
